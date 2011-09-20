95ec531af3f6311e0acebe64dfa718bbc8e47ca2
AlexandrSeleznev <a.seleznev@hotmail.nl>
2011-09-20 13:58:27 +0000

    Scenario changed to workflow.

--- lizard_flooding_worker/client/worker/action_scenario.py
+++ lizard_flooding_worker/client/worker/action_scenario.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python
 # (c) Nelen & Schuurmans.  GPL licensed.
 
-from lizard_flooding_worker.models import Scenario
+from lizard_flooding_worker.models import Workflow
 from lizard_flooding_worker.models import Task
 from lizard_flooding_worker.client.worker.action import Action
 
 import logging
 
 
-class ActionScenario(Action):
+class ActionWorkflow(Action):
 
-    def __init__(self, connection, scenario_id):
+    def __init__(self, connection, workflow_id):
         self.connection = connection
-        self.log = logging.getLogger('lizard-flooding.action.scenario')
-        self.scenario_id = scenario_id
-        self.body = self.retrieve_scenario_options()
+        self.log = logging.getLogger('lizard-flooding.action.workflow')
+        self.workflow_id = workflow_id
+        self.body = self.retrieve_workflow_options()
 
     def callback(self, ch, method, properties, body):
         pass
 
-    def retrieve_scenario_options(self):
-        """Retrieves scenario info from database.
-        1 task_code per scenario
+    def retrieve_workflow_options(self):
+        """Retrieves workflow info from database.
+        1 task_code per workflow
         for example:
         wel (120, 130, (160, 132))
         not (120, 120, 130, (160, 132))"""
-        tasks = Task.objects.all().filter(scenario=self.scenario_id)
-        scenario = Scenario.objects.get(pk=self.scenario_id)
+        tasks = Task.objects.all().filter(workflow=self.workflow_id)
+        workflow = Workflow.objects.get(pk=self.workflow_id)
         option = {}
         instruction = {}
         for task in tasks:
@@ -34,8 +34,8 @@ class ActionScenario(Action):
                 option["curr_task_code"] = task.code
             instruction[task.code] = task.sequence
         option["instruction"] = instruction
-        option["customer_id"] = scenario.customer.id
-        option["scenario_id"] = self.scenario_id
+        option["customer_id"] = workflow.customer.id
+        option["workflow_id"] = self.workflow_id
         option["priority"] = ""
         option["curr_log_level"] = ""
         option["message"] = ""
@@ -43,13 +43,13 @@ class ActionScenario(Action):
         option["next_sequence"] = 1
         return option
 
-    def start_scenario(self, task_code):
+    def start_workflow(self, task_code):
         """Sends trigger and logging messages to broker."""
         channel = self.connection.channel()
         task_queue_options = self.retrieve_queue_options(task_code)
-        self.log.info("Start scenario")
+        self.log.info("Start workflow")
         #send trigger
         #TODO send trigger on the smae way as by action_task
         self.publish_message(channel, task_queue_options, self.body)
-        self.log.info("Started scenario")
+        self.log.info("Started workflow")
         #self.connection.close()
