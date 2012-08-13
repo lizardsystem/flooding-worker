7db17a2b36722c4acc7f5f306fe0bfac2d413ab9
Alexandr Seleznev <alexandr.seleznev@hotmail.nl>
2012-08-13 16:51:09 +0200

    Work in progress.

--- flooding_worker/worker/worker.py
+++ flooding_worker/worker/worker.py
@@ -10,7 +10,7 @@ from multiprocessing import Process
 from flooding_worker.worker.broker_connection import BrokerConnection
 
 
-def start_actin(action):
+def start_action(action):
     action.callback()
 
 
@@ -31,7 +31,7 @@ class Worker():
         try:
             self.channel = self.connection.channel()
             self.channel.basic_qos(prefetch_count=1)
-            self.channel.basic_consume(action.start_action(self.action),
+            self.channel.basic_consume(start_action(self.action),
                                   queue=self.task_code,
                                   no_ack=False)
             self.channel.start_consuming()
